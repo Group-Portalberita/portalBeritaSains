f9a02b1d3c6e09fc825b7b93c0d8602ff39b3a38
mufendew <m.arkanmufadho@gmail.com>
2021-03-04 22:55:00 +0700

    add home page to index html

---
+++ client/js/script.js
@@ -0,0 +1,106 @@
+const baseURL = 'http://localhost:3000'
+$("document").ready(function () {
+    checkLocalStorage()
+
+    $("#btn-login").on("click", (e) => {
+        e.preventDefault();
+        login();
+    })
+
+    $("#btn-logout").on("click", () => {
+        logout();
+    })
+
+    $("#btn-register").on("click", (e) => {
+        e.preventDefault();
+        register();
+    })
+
+    $("#link-register").on("click", (e) => {
+        e.preventDefault();
+        $("#page-login").hide();
+        $("#page-add-todo").hide();
+        $("#page-todos").hide();
+        $("#page-register").show();
+    })
+
+})
+
+function onSignIn(googleUser) {
+    $.ajax({
+        method: "POST",
+        url: baseURL + '/loginGoogle',
+        data: {
+            token: googleUser.getAuthResponse().id_token
+        }
+    })
+        .done((response) => {
+            localStorage.setItem("access_token", response.access_token)
+            checkLocalStorage();
+        })
+        .fail((err) => {
+            console.log(err);
+        })
+        .always(() => {
+            $("#email").val("")
+            $("#password").val("")
+        })
+}
+
+function login() {
+    const email = $("#email").val();
+    const password = $("#password").val();
+
+    $.ajax({
+        url: baseURL + "/login",
+        method: "POST",
+        data: {
+            email,
+            password
+        }
+    })
+        .done((response) => {
+            localStorage.setItem("access_token", response.access_token)
+            checkLocalStorage();
+        })
+        .fail((err) => {
+            console.log(err);
+        })
+        .always(() => {
+            $("#email").val("")
+            $("#password").val("")
+        })
+}
+
+function register() {
+    const email = $("#email-regis").val();
+    const password = $("#password-regis").val();
+
+    $.ajax({
+        url: baseURL + "/register",
+        method: "POST",
+        data: {
+            email,
+            password
+        }
+    })
+        .done((response) => {
+            console.log(response);
+            checkLocalStorage();
+        })
+        .fail((err) => {
+            console.log(err);
+        })
+}
+
+function checkLocalStorage() {
+    if (localStorage.access_token) {
+        $("#page-login").hide();
+        $("#page-register").hide();
+        $("#page-contoh").show();
+    } else {
+        $("#page-login").show();
+        $("#page-register").hide();
+        $("#page-contoh").hide();
+    }
+}
