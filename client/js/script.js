97c5a78d3d6ad1b3494ba6de8f632f344dc5f874
Luthfi Muhaimin <luthfimhmn@gmail.com>
2021-03-05 01:01:59 +0700

    fitur logout

--- client/js/script.js
+++ client/js/script.js
@@ -103,3 +103,12 @@ function checkLocalStorage() {
         $("#homePage").hide();
     }
 }
+
+function logout() {
+    localStorage.removeItem("access_token");
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log("User signed out.");
+    })
+    checkLocalStorage();
+}
